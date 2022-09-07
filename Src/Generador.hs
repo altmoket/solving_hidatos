efab4ed56b8adbfcfd62c60885d6916bb8247af4
copy90471 <copy90471@gmail.com>
2022-09-07 16:05:28 -0400

    Version estable 3

--- Src/Generador.hs
+++ Src/Generador.hs
@@ -18,46 +18,52 @@ comenzar_generacion rows cols valorMinimo valorMaximo digitos
         generacion = generar_sudoku (rows,cols) posMenor posMayor valorMinimo valorMaximo digitos
 
 generar_posicion_aleatoria::Int->Int->Position
-generar_posicion_aleatoria rows cols = 
-  let i = generar_numero_aleatorio 0 rows
-      j = generar_numero_aleatorio 0 cols
-  in Position i j
+generar_posicion_aleatoria rows cols = Position i j
+  where i = generar_numero_aleatorio 0 rows
+        j = generar_numero_aleatorio 0 cols
 
 generar_numero_aleatorio::Int->Int->Int
 generar_numero_aleatorio limiteInferior limiteSuperiorExcluido = c_random limiteInferior limiteSuperiorExcluido
 
-generar_sudoku (rows,cols) posMin@(Position x1 y1) posMax@(Position x2 y2) valorMinimo valorMaximo digitos =
-  let tablero = update_matrix (update_matrix (generar_tablero_vacio rows cols digitos) posMin valorMinimo) posMax valorMaximo
-      sudoku = World tablero posMin posMax
-      solucion@(World matrixSolucion _ _) = solve sudoku
-      matrixInicial = remplazar_casillas_no_visitadas sudoku solucion digitos
-      valor = to_string 0 digitos
-      posiciones = obtener_posicion_casillas matrixInicial valor
-      cantidad = generar_numero_aleatorio 0 (length posiciones)
-      matrixModificada = colocar_valores matrixInicial matrixSolucion posiciones cantidad
-      resultadoFinal = World matrixModificada posMin posMax
-  in 
-    if solucion == Empty then Empty else resultadoFinal
+generar_sudoku (rows,cols) posMin@(Position x1 y1) posMax@(Position x2 y2) valorMinimo valorMaximo digitos = if solucion == Empty then Empty else resultadoFinal
+  where tablero = update_matrix (update_matrix (generar_tablero_vacio rows cols digitos) posMin valorMinimo) posMax valorMaximo
+      
+        valorObstaculo = to_string (-1) digitos
+        valorLibre = to_string 0 digitos
+        minimo = to_int
+
+        cantidadObstaculos = rows * cols - (to_int valorMaximo)
+        
+        tableroObstaculizado = colocar_obstaculos tablero (obtener_posicion_casillas tablero valorLibre) cantidadObstaculos valorObstaculo
+
+        sudoku = World tableroObstaculizado posMin posMax
+        solucion = solve sudoku
+        matrixSolucion = if solucion == Empty then [[]] else (matrix solucion) 
+
+        -- matrixInicial = remplazar_casillas_no_visitadas sudoku solucion digitos
+        posiciones = obtener_posicion_casillas tableroObstaculizado valorLibre
+        cantidadValores = generar_numero_aleatorio 0 (length posiciones)
+        matrixModificada = colocar_valores tableroObstaculizado matrixSolucion posiciones cantidadValores
+        resultadoFinal = World matrixModificada posMin posMax
 
 generar_tablero_vacio rows cols digitos = [ [ [ 'x' | _ <- [1..digitos]] | j <- [0..cols-1] ] | i <- [0..rows-1]]
 
 remplazar_casillas_no_visitadas sudokuOriginal@(World matrixOriginal _ _) sudokuSolucion@(World matrixSolucion _ _) digitos = 
-  let resultado i j = 
-        let casillaNoVisitada = replicate digitos 'x'
-            casillaObstaculo = replicate digitos '-'
-            casillaOriginal = matrixOriginal!!i!!j
-            casillaSolucion = matrixSolucion!!i!!j
-        in if casillaSolucion == casillaNoVisitada then casillaObstaculo
-              else if casillaOriginal == casillaNoVisitada then casillaNoVisitada
-                else casillaOriginal
-      (rows,cols) = get_sudoku_dimensions sudokuOriginal 
-  in [ [ resultado i j | j<-[0..cols-1] ] | i <- [0..rows-1]]
+  [ [ resultado i j | j<-[0..cols-1] ] | i <- [0..rows-1]]
+  where resultado i j = 
+          let casillaNoVisitada = replicate digitos 'x'
+              casillaObstaculo = replicate digitos '-'
+              casillaOriginal = matrixOriginal!!i!!j
+              casillaSolucion = matrixSolucion!!i!!j
+          in if casillaSolucion == casillaNoVisitada then casillaObstaculo
+                else if casillaOriginal == casillaNoVisitada then casillaNoVisitada
+                  else casillaOriginal
+        (rows,cols) = get_sudoku_dimensions sudokuOriginal 
 
 obtener_posicion_casillas::Matrix->String->[Position]
-obtener_posicion_casillas matrix valorBuscado =
-  let (rows,cols) = get_sudoku_dimensions $ World matrix None None
-      posiciones = delete None (nub [ if valor==valorBuscado then (Position i j) else None | i <- [0..rows-1], j<-[0..cols-1], let valor = matrix!!i!!j ])
-  in posiciones
+obtener_posicion_casillas matrix valorBuscado = posiciones
+  where (rows,cols) = get_sudoku_dimensions $ World matrix None None
+        posiciones = delete None (nub [ if valor==valorBuscado then (Position i j) else None | i <- [0..rows-1], j<-[0..cols-1], let valor = matrix!!i!!j ])
 
 colocar_valores::Matrix->Matrix->[Position]->Int->Matrix
 colocar_valores matrix _ _ 0 = matrix
@@ -68,6 +74,18 @@ colocar_valores matrix matrixResultado posiciones cantidad =
       nuevaMatrix = update_matrix matrix posicion (matrixResultado!!i!!j) 
       nuevaCantidad = cantidad - 1
   in colocar_valores nuevaMatrix matrixResultado nuevasPosiciones nuevaCantidad
+
+colocar_obstaculos::Matrix->[Position]->Int->String->Matrix
+colocar_obstaculos matrix _ 0 _ = matrix
+colocar_obstaculos matrix posiciones cantidad valor = 
+  let 
+      index = generar_numero_aleatorio 0 (length posiciones)
+      posicion = posiciones!!index
+      nuevasPosiciones = eliminar_posicion posiciones index 
+      nuevaCantidad = cantidad - 1
+      nuevaMatrix = update_matrix matrix posicion valor
+  in colocar_obstaculos nuevaMatrix nuevasPosiciones nuevaCantidad valor
+  
 
 eliminar_posicion::[a]->Int->[a]
 eliminar_posicion [] _ = []
